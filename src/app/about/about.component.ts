da5df287ab6b584cd8fe2a41de890a39d95ac4c2
maikol <mgarrido2503@gmail.com>
2024-07-04 14:37:20 +0200

    quito los margenes

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -221,13 +221,14 @@ export class AboutComponent implements OnInit, AfterViewInit, OnDestroy  {
     const header_texto_profesion = gsap.to(".header .texto_profesion p", { y: "-100%", duration: 1 });
     const textos_nombres = gsap.to(".texto_nombre", { y: "-100%", duration: 1 , position:'absolute'});
     const header_img_ani1 = gsap.to(".img_header", {left:'50%', x:"-50%",top:'0%', y:"0px", duration: .5, marginTop:'50px' });
-    const header_img_ani2 = gsap.to(".img_header", {position:'relative', duration: 1 , height:'300px', paddingTop:'10px', width:'300px', borderRadius:'100%', background:'#1a1a1a' });
+    const header_img_ani2 = gsap.to(".img_header", {position:'relative', duration: 1 , height:'300px', paddingTop:'10px', width:'300px', borderRadius:'100%', background:'black' });
     const header_texto_sobre_mi = gsap.to(".header .texto_sobre_mi", {opacity:1,position:'relative', duration: .5});
-    const header = gsap.to(".header", {y:"-100%", duration: 2});
+    const header = gsap.to(".header", {y:"-100%", duration: 3});
+    const arrow_container_flecha = gsap.to(".arrow-container .flecha", { duration: 1, opacity:0});
     const skills_section_ani_1 = gsap.to(".skills-section", { duration: 1, opacity:1});
     const skills_section_ani_2 = gsap.to(".skills-section", { duration: 8,  y:'-70%', top:'0%'});
     const skills_section_ani_3 = gsap.to(".skills-section", { duration: 2,opacity:0, x:'-100%'});
-    const cuadro_hijo2_ani1 = gsap.to(".cuadro_hijo2", { duration:1, x:"-100%"});
+    const cuadro_hijo2_ani1 = gsap.to(".cuadro_hijo2", { duration:2, x:"-100%"});
     const img_header_hijo2_ani2 = gsap.to(".img_header_hijo2", { duration:2, top:"-100%"});
     const cuadro_hijo2_ani2 = gsap.to(".cuadro_hijo2", { duration: 2, y:'-40%'});
     
@@ -237,15 +238,15 @@ export class AboutComponent implements OnInit, AfterViewInit, OnDestroy  {
     this.tl.add(header_img_ani1, .1) // sube foto
     this.tl.add(header_img_ani2, .1) // sube foto
     this.tl.add(header_texto_sobre_mi, .2) // aparece texto
-    this.tl.add(header, 1.7) //header sube
+    this.tl.add([header, arrow_container_flecha], 1.7) //header sube
     this.tl.add([skills_section_ani_1], 2)
     this.tl.add([skills_section_ani_2], 3.2)
   
     this.containerSkillBar.forEach((container)=> {
       const innerSkills = container.nativeElement.querySelectorAll('.contenedor-porcentage');
       innerSkills.forEach((innerSkill: HTMLElement, index:number) => {
-        this.delay = 1.9 + index * 0.06
-        const a = gsap.from(innerSkill, {width:0, duration:.4, delay:this.delay, opacity:0});
+        this.delay = 1.8 + index * 0.06
+        const a = gsap.from(innerSkill, {width:0, duration:.6, delay:this.delay, opacity:0});
         this.tl.add(a, this.delay);
     });
   });
